3c3a6a93d5c3a920771e2cdad67e24fda3eba2dd
Gabriela de Moraes da Silva <g.de.moraes.da.silva@avanade.com>
2025-05-07 03:59:42 -0300

    recuperando repo

---
+++ README.md
@@ -0,0 +1,26 @@
+# Build - ok
+
+- A conferir:
+
+actions/checkout <br>
+microsoft/setup-msbuild
+
+Quais as versões mais atualizadas?
+
+- A implementar, caso desejado:
+
+Cache?
+
+# Tests - ok
+
+- Dúvida:
+
+A solução de app e a de teste devem estar dentro da mesma pasta <br>
+e serem refernciadas por src, por exemplo?
+
+- A conferir:
+
+Mudar a chamada de teste para algo mais geral
+
+# SonarQube
+
---
+++ src/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Avanade")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © Avanade 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("558cdd63-1fc5-4b67-9f49-c00da0c53218")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
